9d0305cd317d478c37427f4cc8d8ed35608f1eaf
mahesh <68888458+kiddowhy@users.noreply.github.com>
2024-07-08 23:22:52 +0530

    Initial commit

---
+++ Assets/Scripts/CreateLine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateLine : MonoBehaviour
+{
+    [SerializeField] RectTransform pos1;
+    [SerializeField] RectTransform pos2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+                CreateLineFunc(pos1, pos2);
+        }
+    }
+    void CreateLineFunc(RectTransform cPos1,RectTransform cPos2)
+    {
+        LineRenderer lineR = gameObject.GetComponent<LineRenderer>();
+        lineR.widthMultiplier = 2f;
+        lineR.positionCount = 2;
+        lineR.SetPosition(0, cPos1.position);
+        lineR.SetPosition(1, cPos2.position);
+    }
+}
